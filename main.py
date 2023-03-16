1d54e6768959cad989c93afc84213f382aaf1dba
bitorhugo <vitorhugosantos144@gmail.com>
2023-03-16 18:03:11 +0000

    splitted files per type

--- main.py
+++ main.py
@@ -3,19 +3,26 @@
 import os, pprint
 from encoder import GolombRice
 
-def load(datapath: str, debug=False) -> list[str]:
+def load(datapath: str, debug=False) -> dict[str, list[str]]:
     '''
     Retuns a list of all filename included in corpus sylesia
     '''
-    files = [datapath + f for f in os.listdir(datapath)]
+    dirs = []
+    files = {}
+
+    dirs = os.listdir(datapath)
+    for dir in dirs:
+        files[dir] = os.listdir(datapath + dir)
+
     if debug:
         pprint.pprint(files)
+
     return files
 
     
 def main():
     files = load('data/', debug=True)
-
+    
     gr = GolombRice()
     
     
