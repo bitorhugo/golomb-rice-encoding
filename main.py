0f1a4d3b57c4f787596f9e49ff355cdef54f9457
bitorhugo <vitorhugosantos144@gmail.com>
2023-03-16 16:31:23 +0000

    find data files

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python
 
-def main():
-    print('Hello World')
+import os, pprint
 
-    
+def main():
+    files = [] # corpus sylesia file list
+    for root, _, files in os.walk('data'):
+        files.append(os.path.join(root))
+    files.pop() # remove hanging dir name
+    pprint.pprint(files)
+
 if __name__ == '__main__':
     main()
