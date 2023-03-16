c6ac6211181b64db289c0cd89785f1e023f1da20
bitorhugo <vitorhugosantos144@gmail.com>
2023-03-16 16:55:17 +0000

    encoder class created

---
+++ encoder.py
@@ -0,0 +1,21 @@
+class GolombRice():
+
+
+    def __init__(self) -> None:
+        '''
+        '''
+        pass
+
+
+    def encode(self):
+        '''
+        '''
+        pass
+
+
+    def decode(self):
+        '''
+        '''
+        pass
+
+    
--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python
 
 import os, pprint
+from encoder import GolombRice
 
-def load(datapath: str) -> list[str]:
+def load(datapath: str, debug=False) -> list[str]:
     '''
     Retuns a list of all filename included in corpus sylesia
     '''
-    return [datapath + f for f in os.listdir(datapath)]
+    files = [datapath + f for f in os.listdir(datapath)]
+    if debug:
+        pprint.pprint(files)
+    return files
 
     
 def main():
-    files = load('data/')
-    print(files)
+    files = load('data/', debug=True)
 
-
+    gr = GolombRice()
+    
     
 if __name__ == '__main__':
     main()
