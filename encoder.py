3bbb1b2eebca3f86ecf228613dea8c1ec814760d
bitorhugo <vitorhugosantos144@gmail.com>
2023-03-23 13:36:33 +0000

    ADD: generate byte sequence for input file

--- encoder.py
+++ encoder.py
@@ -1,12 +1,34 @@
+import re
+
 class GolombRice():
+    
 
-
-    def __init__(self) -> None:
+    def __init__(self, file: str, m: int=16, debug: bool=False) -> None:
         '''
         '''
-        pass
+        self.byte_seq = self.__byte_seq(file, debug)
+        self.m = m
 
 
+    def __byte_seq(self, file: str, debug: bool=False) -> list[int]:
+        seq = []
+        with open(file, 'rb') as f:
+            b = f.read(1)
+            i = 0
+            while(b):
+                byte = ord(b)
+                bits = bin(byte)[2:].rjust(8, '0') # rjust will pad 0's to the left
+                seq.append(byte)
+                i += 1
+                if i == 200:
+                    break
+                if debug:
+                    print(f'byte: {byte}')
+                    print(f'bits: {bits}')
+                    print('-------------')
+                b = f.read(1)
+        return seq
+            
     def encode(self):
         '''
         '''
@@ -18,4 +40,7 @@ class GolombRice():
         '''
         pass
 
-    
+    def zero_prob(self) -> float:
+        for byte in self.byte_seq:
+            bits = bin(byte)[2:].rjust(8, '0')
+        return 0.0
