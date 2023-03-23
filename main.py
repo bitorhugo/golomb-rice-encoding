8d7bfcc00ae43b21bc23aa5bfe92c9cc372a17ee
bitorhugo <vitorhugosantos144@gmail.com>
2023-03-23 04:09:24 +0000

    TESTING: converting bytes to bits

--- main.py
+++ main.py
@@ -27,8 +27,13 @@ def main():
         b = f.read(1)
         i = 0
         while(b):
-            print(f'byte: {b}')
-            print(f'from_int: {int.from_bytes(b, "big")}')
+            byte = ord(b)
+            bits = bin(byte)[2:].rjust(8, '0') # rjust will pad 0's to the left
+            int_value = ord(b)
+            print(f'byte: {byte}')
+            print(f'bits: {bits}')
+            print(f'int: {int_value}')
+            print('-------------')
             b = f.read(1)
             if i == 20:
                 break
