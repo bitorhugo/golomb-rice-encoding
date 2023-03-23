57ecf1eed6daaa3d5a2113d8ece57feadfd7df5c
bitorhugo <vitorhugosantos144@gmail.com>
2023-03-23 13:37:14 +0000

    TESTING: Utilizing golomb-rice object to encode

--- main.py
+++ main.py
@@ -19,26 +19,10 @@ def load(datapath: str, debug=False) -> dict[str, list[str]]:
     
 def main():
     files = load('data/', debug=True)
-
     images = files['pic']
     img = images.pop()
 
-    with open(img, 'rb') as f:
-        b = f.read(1)
-        i = 0
-        while(b):
-            byte = ord(b)
-            bits = bin(byte)[2:].rjust(8, '0') # rjust will pad 0's to the left
-            int_value = ord(b)
-            print(f'byte: {byte}')
-            print(f'bits: {bits}')
-            print(f'int: {int_value}')
-            print('-------------')
-            b = f.read(1)
-            if i == 20:
-                break
-            i += 1
-
-            
+    gr = GolombRice(img, debug=True)
+    print (f'zero_prob: {gr.zero_prob()}')
 if __name__ == '__main__':
     main()
