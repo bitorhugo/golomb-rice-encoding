461af12385dea00477fc7d6e89c823d7b89d1cb8
bitorhugo <vitorhugosantos144@gmail.com>
2023-03-23 14:43:02 +0000

    ADD: Calculate m and zero_prob

--- encoder.py
+++ encoder.py
@@ -1,13 +1,11 @@
-import re
-
+import math
 class GolombRice():
     
 
-    def __init__(self, file: str, m: int=16, debug: bool=False) -> None:
+    def __init__(self, file: str, debug: bool=False) -> None:
         '''
         '''
         self.byte_seq = self.__byte_seq(file, debug)
-        self.m = m
 
 
     def __byte_seq(self, file: str, debug: bool=False) -> list[int]:
@@ -17,18 +15,19 @@ class GolombRice():
             i = 0
             while(b):
                 byte = ord(b)
-                bits = bin(byte)[2:].rjust(8, '0') # rjust will pad 0's to the left
                 seq.append(byte)
-                i += 1
-                if i == 200:
-                    break
                 if debug:
+                    bits = bin(byte)[2:].rjust(8, '0') # rjust will pad 0's to the left
                     print(f'byte: {byte}')
                     print(f'bits: {bits}')
                     print('-------------')
+                    i += 1
+                    if i == 200:
+                        break
                 b = f.read(1)
         return seq
             
+
     def encode(self):
         '''
         '''
@@ -40,7 +39,27 @@ class GolombRice():
         '''
         pass
 
-    def zero_prob(self) -> float:
+
+    def m(self) -> float:
+        '''
+        m is a formula that will be rounded to the nearest base 2 exponential value
+        m = -log(1+zero_prob)/log(zero_prob)
+        '''
+        zero_prob = self.zero_prob()
+        return self.__next_power_of_two(-(math.log(1 + zero_prob) / math.log(zero_prob)))
+
+    
+    def __next_power_of_two(self, x: float) -> int:
+        return 1 if x == 0 else 2**math.ceil(math.log2(x))
+
+    
+    def zero_prob(self, debug: bool=False) -> float:
+        count = 0
         for byte in self.byte_seq:
             bits = bin(byte)[2:].rjust(8, '0')
-        return 0.0
+            count += bits.count('0')
+        if debug:
+            print(f'zero_count: {count}')
+            print(f'total_bits: {len(self.byte_seq) * 8}')
+            print(f'prob: {count/(len(self.byte_seq) * 8)}')
+        return count/(len(self.byte_seq) * 8)
