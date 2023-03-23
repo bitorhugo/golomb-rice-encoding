df8be23fa4feaa28cb60aa088958f82bc6889eb9
bitorhugo <vitorhugosantos144@gmail.com>
2023-03-23 03:09:52 +0000

    ADD: refactored path to each file

--- main.py
+++ main.py
@@ -7,24 +7,33 @@ def load(datapath: str, debug=False) -> dict[str, list[str]]:
     '''
     Retuns a list of all filename included in corpus sylesia
     '''
-    dirs = []
     files = {}
-
     dirs = os.listdir(datapath)
     for dir in dirs:
-        files[dir] = os.listdir(datapath + dir)
+        files[dir] = ['data/' + dir + "/" + f for f in os.listdir(datapath + dir)]
 
     if debug:
         pprint.pprint(files)
-
     return files
 
     
 def main():
     files = load('data/', debug=True)
-    
-    gr = GolombRice()
-    
-    
+
+    images = files['pic']
+    img = images.pop()
+
+    with open(img, 'rb') as f:
+        b = f.read(1)
+        i = 0
+        while(b):
+            print(f'byte: {b}')
+            print(f'from_int: {int.from_bytes(b, "big")}')
+            b = f.read(1)
+            if i == 50:
+                break
+            i += 1
+
+            
 if __name__ == '__main__':
     main()
