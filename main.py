70c88ad91bcede79089f87b5591bd3f429f19365
bitorhugo <vitorhugosantos144@gmail.com>
2023-03-24 02:08:19 +0000

    TESTING: computing sequence of zeros

--- main.py
+++ main.py
@@ -10,23 +10,28 @@ def load(datapath: str, debug=False) -> dict[str, list[str]]:
     files = {}
     dirs = os.listdir(datapath)
     for dir in dirs:
-        files[dir] = ['data/' + dir + "/" + f for f in os.listdir(datapath + dir)]
+        files[dir] = ['data/corpus-silesia/' + dir + "/" + f for f in os.listdir(datapath + dir)]
     if debug:
         pprint.pprint(files)
     return files
 
     
 def main():
-    files = load('data/', debug=True)
+    files = load('data/corpus-silesia/', debug=True)
     images = files['pic']
     img = images.pop()
     
-    gr = GolombRice(img, debug=True)
+    gr = GolombRice(img, debug=False)
     # TODO: refactor using bits and not bytes
     # print(f'm: {gr.m()}')
-    print (f'q: {gr.q(8, 2)}')
+    # print (f'q: {gr.q(8, 2)}')
+    print(gr.zero_seq())
 
 
     
 if __name__ == '__main__':
     main()
+
+
+
+# https://bitstring.readthedocs.io/en/stable/slicing.html
