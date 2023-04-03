88fc127b9bd968cb7418dcef62ee2e553373aaea
bitorhugo <vitorhugosantos144@gmail.com>
2023-04-03 22:09:33 +0100

    ADD: encoding

--- encoder.py
+++ encoder.py
@@ -1,7 +1,7 @@
-import math, bitstring, csv, time
+import os, math, bitstring, csv, time
+
 
 class GolombRice():
-
     
     __ZERO_SEQ_PATH = 'data/helper/zero-seq.csv'
     __zero_count = 0
@@ -10,9 +10,9 @@ class GolombRice():
     def __init__(self, file: str, debug: bool=False) -> None:
         '''
         '''
-        self.__bitstream = bitstring.BitArray(filename=file) 
+        self.__bitstream = bitstring.BitArray(filename=file)
         self.__zero_seq(debug)
-
+        
         
     def __byte_seq(self, file: str, debug: bool=False) -> tuple[list[int], list[int]]:
         seq = []
@@ -37,41 +37,84 @@ class GolombRice():
         return (seq, seq_bits)
             
 
-    def encode(self):
+    def encode(self, debug: bool=False):
         '''
+        Encodes input stream
         '''
-        pass
+        # fist calculate p(0)
+        p = self.zero_prob()
+        if debug:
+            print(f'p={p}')
+        # second calculate m()
+        m = self.m()
+        if debug:
+            print (f'm={m}')
+        with open(self.__ZERO_SEQ_PATH) as f:
+            reader = csv.reader(f)
+            # discard csb headers
+            reader.__next__()
+            n = int(reader.__next__().pop())
+            if debug:
+                print(f'n={n}')
+            # third calculate q iteratively
+            q_int, q_unar = self.q(n, m) # q is a tuple containing its integer value and its unary representation
+            if debug:
+                print(f'q={(q_int, q_unar)}')
+            # fourth calculate remainder with c bits
+            r = self.r(n, q_int, m, debug=True)
+            # lastly concat q and r
+            cod = str(q_unar) + str(r)
+            if (debug):
+                print(f'cod={cod}')
 
-
+                
     def decode(self):
         '''
+        Decodes output stream
         '''
         pass
 
 
-    def r(self):
-        pass
+    def c(self, m: int) -> int:
+        '''
+        Calculates the amount of bits necessary to represent r
+        '''
+        return math.ceil(math.log2(m))
+
+        
+    def r(self, n: int, q: int, m: int, debug: bool=False) -> int:
+        '''
+        Calculates remainder and returns it in 'c' amount of bits
+        '''
+        r = str(n - (q * m))
+        c = self.c(m)
+        padding = len(r) - c
+        r = r.rjust(padding, '0')
+        if debug:
+            print(f'r={r}')
+        return int(r)
+
     
-    def q(self, n: int, m: float) -> int:
+    def q(self, n: int, m: float) -> tuple[int, str]:
         '''
         Calculates quoficient and returns it as unary
         q = n / m, rounded to the floor
         '''
         q = math.floor( n/m )
-        value = ''
+        unary = ''
         for _ in range(q):
-            value += '1'
-        value += '0'
-        return int(value)
+            unary += '1'
+        unary += '0'
+        return (q, unary)
 
-    
-    def m(self) -> float:
+
+    def m(self) -> int:
         '''
         m is a formula that will be rounded to the nearest base 2 exponential value
         m = -log(1+zero_prob)/log(zero_prob)
         '''
         p = self.zero_prob()
-        return self.__next_power_of_two(-math.log(1 + p) / math.log(p))
+        return math.ceil(self.__next_power_of_two(-math.log(1 + p) / math.log(p)))
 
     
     def __next_power_of_two(self, x: float) -> int:
@@ -80,11 +123,15 @@ class GolombRice():
 
     def zero_prob(self) -> float:
         return self.__zero_count / len(self.__bitstream.bin)
-    
+
+    def zero_count(self):
+        b = self.__bitstream.bin
+        pass
         
     def __zero_seq(self, debug: bool):
         start = time.time()
         b = self.__bitstream.bin
+        # check to see if file exists before computing sequence of zeros
         with open(self.__ZERO_SEQ_PATH, 'w+') as f:
             w = csv.writer(f)
             w.writerow(['number-of-zeros'])
@@ -95,6 +142,6 @@ class GolombRice():
         end = time.time()
         elapsed = end - start
         if debug:
-            print(elapsed)
+            print(f'elapsed:{elapsed}')
             
 # https://bitstring.readthedocs.io/en/stable/slicing.html
